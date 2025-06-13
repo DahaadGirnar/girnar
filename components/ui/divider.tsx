db59a6ea8611175f67b3b14d307e09657a854534
HotShot0901 <contact.hot.shor.1234@gmail.com>
2025-06-13 21:30:38 +0530

    new Look

--- components/ui/divider.tsx
+++
@@ -1,7 +0,0 @@
-const Divider = () => {
-    return (
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-    );
-};
-
-export default Divider;
